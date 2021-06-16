9e77a7b244c4950e30788dd1b33c5f866ec0d795
Deovyonne Patterson <85905465+DeoPatt@users.noreply.github.com>
2021-06-16 22:12:39 +0000

    final changes

--- main.cs
+++ main.cs
@@ -8,6 +8,14 @@ class MainClass {
     Console.WriteLine("Enter Another Number");
     int num2 = Convert.ToInt32(Console.ReadLine());
     int sum = num1 + num2;
-    Console.WriteLine()
+    Console.WriteLine("sum = " +  sum);
+    int diff = num1 - num2;
+    Console.WriteLine("Diff = " + diff);
+    int pro = num1 * num2;
+    Console.WriteLine("Product = " + pro);
+    int quo = num1 / num2;
+    Console.WriteLine("Quotient = " + quo);
+    int remain = num1 % num2;
+    Console.WriteLine("Remainder = " + remain);
   }
 }
